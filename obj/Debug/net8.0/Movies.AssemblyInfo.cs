90eb840692c0d78034e1cb99187e440633396a96
dx-daniel <desarrolloti2@disex.com.co>
2025-11-28 20:34:13 -0500

    some no relative changes

--- obj/Debug/net8.0/Movies.AssemblyInfo.cs
+++ obj/Debug/net8.0/Movies.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Movies")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d53c4cddc512c6f9fe28a5cb9ec61c34fc9bf8aa")]
 [assembly: System.Reflection.AssemblyProductAttribute("Movies")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Movies")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
