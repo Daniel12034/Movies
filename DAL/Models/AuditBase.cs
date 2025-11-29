b22929fdee5a52d21599ce1bfae8bda77a166705
dx-daniel <desarrolloti2@disex.com.co>
2025-11-28 20:28:58 -0500

    model bases for categories first table

---
+++ DAL/Models/AuditBase.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Movies.DAL.Models
+{
+    public class AuditBase
+    {
+        [Key]
+        public virtual int Id { get; set; }
+
+        public virtual DateTime CreatedAt { get; set; }
+
+        public virtual DateTime UpdatedAt { get; set; }
+    }
+}
---
+++ DAL/Models/Category.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Movies.DAL.Models
+{
+    public class Category : AuditBase
+    {
+        [Required]
+        [Display(Name = "Categoria")]
+        public int Name { get; set; }
+    }
+}
