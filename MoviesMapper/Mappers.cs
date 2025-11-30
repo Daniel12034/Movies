3369378279d38e7b9e6c07802e12029f336c2e29
Daniel12034 <gilmazodaniel123@gmail.com>
2025-11-30 16:34:34 -0500

    mapper structure

---
+++ MoviesMapper/Mappers.cs
@@ -0,0 +1,17 @@
+﻿using AutoMapper;
+using Movies.DAL.Models;
+using Movies.DAL.Models.Dtos;
+
+namespace Movies.MoviesMapper
+{
+    public class Mappers : Profile
+    {
+
+        public Mappers()
+        {
+            CreateMap<Category, CategoryDto>().ReverseMap();
+
+            CreateMap<Category, CategoryCreateDto>().ReverseMap();
+        }
+    }
+}
