206b90d299aebaeae3ac4606662aed2758f5cef6
Daniel12034 <gilmazodaniel123@gmail.com>
2025-11-30 17:18:15 -0500

    services changes

---
+++ Service/CategoryService.cs
@@ -0,0 +1,57 @@
+﻿using Movies.DAL.Models;
+using Movies.Service.IServices;
+using Movies.Repository.IRepository;
+using AutoMapper;
+using Movies.DAL.Models.Dtos;
+
+namespace Movies.Service
+{
+    public class CategoryService : ICategoryService
+    {
+        private readonly ICategoryRepository _categoryRepository;
+        private readonly IMapper _mapper;
+
+        public CategoryService(ICategoryRepository categoryRepository, IMapper mapper)
+        {
+            _categoryRepository = categoryRepository;
+            _mapper = mapper;
+        }
+
+        public Task<bool> CreateCategoryAsync(CategoryDto categoryDto)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> DeleteCategoryAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<CategoryDto> GetCategoryAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<ICollection<CategoryDto>> GetCategoriesAsync()
+        {
+            var categories = await _categoryRepository.GetCategoriesAsync(); //Calling the method from repository
+
+            return _mapper.Map<ICollection<CategoryDto>>(categories); //Mapping the data to DTO
+        }
+
+        public Task<bool> UpdateCategoryAsync(CategoryDto categoryDto)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> CategoryExistsByIdAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> CategoryExistsByNameAsync(string name)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Service/IServices/ICategoryService.cs
@@ -0,0 +1,17 @@
+﻿using Movies.DAL.Models;
+using Movies.DAL.Models.Dtos;
+
+namespace Movies.Service.IServices
+{
+    public interface ICategoryService
+    {
+        Task<ICollection<CategoryDto>> GetCategoriesAsync(); //Returns a list of categories
+        Task<CategoryDto> GetCategoryAsync(int id); //Returns a specific category register
+        Task<bool> CategoryExistsByIdAsync(int id); //Returns a flag indicating a category exists by it's ID
+        Task<bool> CategoryExistsByNameAsync(string name); //Returns a flag indicating a category exists by it's name
+        Task<bool> CreateCategoryAsync(CategoryDto category); //Create a category 
+        Task<bool> UpdateCategoryAsync(CategoryDto category); //Update a category 
+        Task<bool> DeleteCategoryAsync(int id); //Delete a category 
+
+    }
+}
