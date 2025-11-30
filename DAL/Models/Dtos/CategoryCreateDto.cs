23fd6a072b947bcdbfb2119a639a46e52a8d677a
Daniel12034 <gilmazodaniel123@gmail.com>
2025-11-30 16:34:16 -0500

    dto's functionality

---
+++ DAL/Models/Dtos/CategoryCreateDto.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Movies.DAL.Models.Dtos
+{
+    public class CategoryCreateDto
+    {
+        [Required(ErrorMessage = "El nombre de la categoria es obligatorio")]
+        [MaxLength(100, ErrorMessage = "El numero maximo de caracteres es de 100.")]
+        public string Name { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime UpdateddAt { get; set; }
+    }
+}
---
+++ DAL/Models/Dtos/CategoryDto.cs
@@ -0,0 +1,10 @@
+﻿namespace Movies.DAL.Models.Dtos
+{
+    public class CategoryDto
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime UpdateddAt { get; set; }
+    }
+}
