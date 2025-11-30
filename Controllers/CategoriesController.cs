f603dc3fa70fc8d57a955d5a3c153404c5c156cb
Daniel12034 <gilmazodaniel123@gmail.com>
2025-11-30 17:18:04 -0500

    controllers changes

---
+++ Controllers/CategoriesController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Movies.DAL.Models.Dtos;
+using Movies.Service.IServices;
+
+namespace Movies.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CategoriesController : ControllerBase
+    {
+        private readonly ICategoryService _categoryService;
+        public CategoriesController(ICategoryService categoryService)
+        {
+            _categoryService = categoryService;
+        }
+
+        [HttpGet]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        public async Task<ActionResult<ICollection<CategoryDto>>> GetCategoriesAsync()
+        {
+            var categories = await _categoryService.GetCategoriesAsync();
+            return Ok(categories);
+        }
+    }
+}
