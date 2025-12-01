825d6cbef2cfce82326b2855e896d6f1b805501e
Daniel12034 <gilmazodaniel123@gmail.com>
2025-11-30 20:28:16 -0500

    controller implementation of POST endpoint

--- Controllers/CategoriesController.cs
+++ Controllers/CategoriesController.cs
@@ -14,7 +14,7 @@ namespace Movies.Controllers
             _categoryService = categoryService;
         }
 
-        [HttpGet]
+        [HttpGet(Name = "GetCategoriesAsync")]
         [ProducesResponseType(StatusCodes.Status200OK)]
         [ProducesResponseType(StatusCodes.Status500InternalServerError)]
         [ProducesResponseType(StatusCodes.Status400BadRequest)]
@@ -22,6 +22,45 @@ namespace Movies.Controllers
         {
             var categories = await _categoryService.GetCategoriesAsync();
             return Ok(categories);
+        }
+
+        [HttpGet("{id:int}", Name = "GetCategoryAsync")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public async Task<ActionResult<CategoryDto>> GetCategoryAsync(int id)
+        {
+            var categoryDto = await _categoryService.GetCategoryAsync(id);
+            return Ok(categoryDto);
+        }
+
+        [HttpPost(Name = "CreateCategoryAsync")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status201Created)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status409Conflict)]
+        public async Task<ActionResult<CategoryDto>> CreateCategoryAsync([FromBody] CategoryCreateDto categoryCreateDto)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+            try
+            {
+                var createdCategory = await _categoryService.CreateCategoryAsync(categoryCreateDto);
+
+                return CreatedAtRoute("GetCategoryAsync", new { id = createdCategory.Id }, createdCategory);
+            }
+            catch(InvalidOperationException ex) when (ex.Message.Contains("Yaa existe"))
+            {
+                return Conflict(new {ex.Message});
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, ex.Message);
+            }
         }
     }
 }
