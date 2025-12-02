e46c44fbad21b6a43f0b87b858d64764a7081dee
Daniel12034 <gilmazodaniel123@gmail.com>
2025-12-01 20:07:26 -0500

    i repository for the movie table

---
+++ Repository/IRepository/IMovieRepository.cs
@@ -0,0 +1,16 @@
+﻿
+using Movies.DAL.Models;
+
+namespace Movies.Repository.IRepository
+{
+    public interface ICategoryRepository
+    {
+        Task<ICollection<Category>> GetCategoriesAsync(); //Returns a list of categories
+        Task<Category> GetCategoryAsync(int id); //Returns a specific category register
+        Task<bool> CategoryExistsByIdAsync(int id); //Returns a flag indicating a category exists by it's ID
+        Task<bool> CategoryExistsByNameAsync(string name); //Returns a flag indicating a category exists by it's name
+        Task<bool> CreateCategoryAsync(Category category); //Create a category 
+        Task<bool> UpdateCategoryAsync(Category category); //Update a category 
+        Task<bool> DeleteCategoryAsync(int id); //Delete a category 
+    }
+}
